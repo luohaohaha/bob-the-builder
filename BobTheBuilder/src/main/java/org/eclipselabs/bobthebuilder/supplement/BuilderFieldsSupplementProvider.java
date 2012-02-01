65ace91ceacfb26d9c4569bdf15db1d7cd76515f
Nicolas.Capdevila <Nicolas.Capdevila@gmail.com>
2012-02-01 13:13:29 +0000

    One flattener less to go. The remaining one is the one that calculates extra with methods.

--- BobTheBuilder/src/main/java/org/eclipselabs/bobthebuilder/supplement/BuilderFieldsSupplementProvider.java
+++ BobTheBuilder/src/main/java/org/eclipselabs/bobthebuilder/supplement/BuilderFieldsSupplementProvider.java
@@ -1,30 +1,103 @@
 package org.eclipselabs.bobthebuilder.supplement;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Set;
+
+import javax.inject.Inject;
 
 import org.apache.commons.lang.Validate;
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipselabs.bobthebuilder.mapper.eclipse.BuilderTypeMapper;
 import org.eclipselabs.bobthebuilder.model.BuilderType;
 import org.eclipselabs.bobthebuilder.model.Field;
 import org.eclipselabs.bobthebuilder.model.MainType;
 
 public class BuilderFieldsSupplementProvider {
+  private final BuilderTypeMapper builderTypeMapper;
 
-  public Set<Field> supplement(MainType mainType) {
-    Validate.notNull(mainType, "mainType may not be null");
+  @Inject
+  public BuilderFieldsSupplementProvider(BuilderTypeMapper builderTypeMapper) {
+    this.builderTypeMapper = builderTypeMapper;
+  }
 
-    BuilderType builderType = mainType.getBuilderType();
-    if (builderType == null || builderType.getBuilderFields().isEmpty()) {
-      return Collections.<Field> emptySet();
+  public Collection<Field> supplement(MainType mainType) throws JavaModelException {
+    return new MappedFieldSupplementor().findSupplement(mainType);
+  }
+
+  public Collection<IField> supplement(IType mainType) throws JavaModelException {
+    return new RawFieldSupplementor(builderTypeMapper).findSupplement(mainType);
+  }
+
+  static class MappedFieldSupplementor extends FieldSupplementor<MainType, BuilderType, Field> {
+
+    @Override
+    protected Collection<Field> getMainTypeFields(MainType mainType) {
+      return mainType.getFields();
     }
-    else {
-      Set<Field> builderFields = builderType.getBuilderFields();
-      Set<Field> copyOfBuilderFields = new HashSet<Field>();
-      copyOfBuilderFields.addAll(builderFields);
-      copyOfBuilderFields.removeAll(mainType.getFields());
-      return Collections.unmodifiableSet(copyOfBuilderFields);
+
+    @Override
+    protected Collection<Field> getBuilderFields(BuilderType builderType) {
+      return builderType.getBuilderFields();
     }
+
+    @Override
+    protected BuilderType getBuilderType(MainType mainType) {
+      return mainType.getBuilderType();
+    }
+
+  }
+
+  static class RawFieldSupplementor extends FieldSupplementor<IType, IType, IField> {
+
+    private final BuilderTypeMapper builderTypeMapper;
+
+    public RawFieldSupplementor(BuilderTypeMapper builderTypeMapper) {
+      this.builderTypeMapper = builderTypeMapper;
+    }
+
+    @Override
+    protected Collection<IField> getMainTypeFields(IType mainType) throws JavaModelException {
+      return Arrays.asList(mainType.getFields());
+    }
+
+    @Override
+    protected Collection<IField> getBuilderFields(IType builderType) throws JavaModelException {
+      return Arrays.asList(builderType.getFields());
+    }
+
+    @Override
+    protected IType getBuilderType(IType mainType) throws JavaModelException {
+      IType builderType = builderTypeMapper.findBuilderType(mainType);
+      return builderType;
+    }
+
+  }
+
+  static abstract class FieldSupplementor<M, B, F> {
+    Collection<F> findSupplement(M mainType) throws JavaModelException {
+      Validate.notNull(mainType, "mainType may not be null");
+      B builderType = getBuilderType(mainType);
+      if (builderType == null || getBuilderFields(builderType).isEmpty()) {
+        return Collections.<F> emptySet();
+      }
+      else {
+        Collection<F> builderFields = getBuilderFields(builderType);
+        Collection<F> copyOfBuilderFields = new HashSet<F>();
+        copyOfBuilderFields.addAll(builderFields);
+        copyOfBuilderFields.removeAll(getMainTypeFields(mainType));
+        return Collections.unmodifiableCollection(copyOfBuilderFields);
+      }
+    }
+
+    protected abstract Collection<F> getMainTypeFields(M mainType) throws JavaModelException;
+
+    protected abstract Collection<F> getBuilderFields(B builderType) throws JavaModelException;
+
+    protected abstract B getBuilderType(M mainType) throws JavaModelException;
   }
 
 }
